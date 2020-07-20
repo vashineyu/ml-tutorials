285ec1453c20899d797087c44146e440328b2143
vashineyu <vashineyu@gmail.com>
2020-07-20 23:49:38 +0800

    update focal loss

---
+++ basic_performance_evaluation_classification/losses.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+from tensorflow.keras import backend as K
+
+def categorical_focal_loss(alpha, gamma=2.):
+    alpha = np.array(alpha, dtype=np.float32)
+    def categorical_focal_loss_fixed(y_true, y_pred):
+        """
+        :param y_true: A tensor of the same shape as `y_pred`
+        :param y_pred: A tensor resulting from a softmax
+        :return: Output tensor.
+        """
+        # Clip the prediction value to prevent NaN's and Inf's
+        epsilon = K.epsilon()
+        y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+
+        # Calculate Cross Entropy
+        cross_entropy = -y_true * K.log(y_pred)
+
+        # Calculate Focal Loss
+        loss = alpha * K.pow(1 - y_pred, gamma) * cross_entropy
+
+        # Compute mean loss in mini_batch
+        return K.mean(K.sum(loss, axis=-1))
+
+    return categorical_focal_loss_fixed
